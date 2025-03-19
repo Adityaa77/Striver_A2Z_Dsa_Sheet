ad344aaf45b082031a0c0f05a7f485a56d8ac1d9
Aditya Singh <127761401+Adityaa77@users.noreply.github.com>
2025-03-19 12:34:08 +0530

    Updated if_else_problems.cpp

--- Basics/if_else_problems.cpp
+++ Basics/if_else_problems.cpp
@@ -1,1 +1,38 @@
+//if else problem 1
+int age;
+cin>>age;
+cout<<"Enter age-"<<age<<endl;
+ if (age>=18){
+    cout<<"you are an adult";
+ }
+ else{
+    cout<<"you are not an adult";
+ }
+ 
+
+ //if else problem 2
+int marks;
+cin>>marks;
+cout<<"Marks-"<<marks<<endl;
+if (marks<25){
+   cout<<"You got F grade";
+}
+else if(marks<44 & marks>25){
+   cout<<"You got E grade";
+}
+else if(marks<49 & marks>45){
+   cout<<"You got D grade";
+}
+else if(marks<59 & marks>50){
+   cout<<"You got C grade";
+}
+else if(marks<79 & marks>60){
+   cout<<"You got B grade";
+}
+else if(marks<=100 & marks>80){
+   cout<<"You got A grade";
+}
+else if(marks>100){
+   cout<<"Error";
+}
 
