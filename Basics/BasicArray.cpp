7bfee11d8c91139804c3a6418691938ac5f8e28a
Aditya Singh <127761401+Adityaa77@users.noreply.github.com>
2025-03-19 12:40:07 +0530

    Added BasicArray.cpp

---
+++ Basics/BasicArray.cpp
@@ -0,0 +1,7 @@
+//ARRAY BASICS
+//make an array with 5 row and 4 columns and change the 3rd rows 4thcolumns element
+int arr[5][4];//array created
+arr[3][4]=27;//element in array
+cout<<"initial value-"<<arr[3][4]<<endl;//showing element in array
+arr[3][4]=77;//changing value
+cout<<"changed value-"<<arr[3][4];//printing the changed value
