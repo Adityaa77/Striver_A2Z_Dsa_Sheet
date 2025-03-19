b44bd3c0088464eba44afa052aaef555a509f9aa
Aditya Singh <127761401+Adityaa77@users.noreply.github.com>
2025-03-19 12:43:45 +0530

    Added Basic Functions.cpp

---
+++ Basics/Basic Functions.cpp
@@ -0,0 +1,30 @@
+ //Functions
+//void 
+void printName(string name) {
+   cout <<"Hey "<<name<<endl;
+}
+int main() {
+  string name1;
+  cin>>name1;
+  printName(name1);
+  
+  string name2;
+  cin>>name2;
+  printName(name2);
+  return 0;
+}
+
+//addition using void
+int num1;
+int num2;
+void printAdd(double num){
+   cout<<num1+num2<<endl;
+}
+int main(){
+
+int add;
+cin>>num1;
+cin>>num2;
+printAdd(add);
+return 0;
+}
