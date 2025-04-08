4cc122510b6517ccc30892b12d17c1275c3e6186
Aditya Singh <127761401+Adityaa77@users.noreply.github.com>
2025-04-08 17:49:58 +0530

    Update BasicRecursion.cpp

--- Basics/Recursion/BasicRecursion.cpp
+++ Basics/Recursion/BasicRecursion.cpp
@@ -5,7 +5,7 @@ void setupIO() {
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
 }
-/*int cnt = 0;
+int cnt = 0;
 
 void print() {
     if (cnt == 9) return;     // base case
@@ -18,59 +18,5 @@ void print() {
 int main() {
     setupIO();
     print();  // call recursive function
-    return 0;
-}*/
-
-//Basic Recursion Problems
-//Print name 5 times
-/*int num=0;
-
-void name(){
-    if(num==5)return;
-    cout<<"Aditya"<<endl;
-    num++;
-    name();
-}
-int main(){
-    setupIO();
-    cout<<"Printing Name 5 TImes-"<<endl;
-    name();
-    return 0;
-}*/
-//Now printing number 1 to n
-/*int num=1;
-void printnum(int n){
-
-    if(num>n)return;
-    cout<<num<<endl;
-    num++;
-  
-    printnum(n);
-    
-}
-int main(){
-    setupIO();
-    int n;
-    cout<<"printing number 1 to n(so give input n)"<<endl;
-    cin>>n;
-    printnum(n);
-    return 0;
-}*/
-int num=0;
-void printnum(int n){
-
-    if(num>=n)return;
-    cout<<n-num<<endl;
-    num++;
-  
-    printnum(n);
-    
-}
-int main(){
-    setupIO();
-    int n;
-    cout<<"printing number n to 1"<<endl;
-    cin>>n;
-    printnum(n);
     return 0;
 }
