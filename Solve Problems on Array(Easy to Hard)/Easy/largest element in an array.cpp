33dfdd15ea3cffd01de7d51d1affd0df927920c7
Aditya Singh <127761401+Adityaa77@users.noreply.github.com>
2025-04-25 17:59:02 +0530

    Update largest element in an array.cpp

--- Solve Problems on Array(Easy to Hard)/Easy/largest element in an array.cpp
+++ Solve Problems on Array(Easy to Hard)/Easy/largest element in an array.cpp
@@ -1,1 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Function to find the largest element in the array
+int largestElement(vector<int>& nums) {
+    int largest = nums[0];  // Initialize with the first element
+    for (int i = 1; i < nums.size(); i++) {
+        if (nums[i] > largest) {
+            largest = nums[i];
+        }
+    }
+    return largest;
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<int> nums(n);
+    for (int i = 0; i < n; i++) {
+        cin >> nums[i];
+    }
+
+    cout << largestElement(nums) << endl;
+
+    return 0;
+}
 
